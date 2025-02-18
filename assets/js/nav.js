3f07c2fa0410b381621dbd818945e29881fbe3f1
Stephen Wildrick <swildrick@gmail.com>
2025-02-17 20:08:22 -0700

    adding pages

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,28 +1,24 @@
-$('.menu-btn').on('click', function() {
-
-    var elem = $(this),
-        item = $('.menu__item'),
-        active = 'is-active',
-        play = 'menu__item--play';
-
-    if (  elem.hasClass(active) ) {
-        elem.removeClass(active);
-        $(item.get().reverse()).each(function(i) {
-            var row = $(this);
-                setTimeout(function() {
-                    row.removeClass(play);
-            }, 50*i);
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".nav-link").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 2000, "easeOutExpo", function() {
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          //window.location.hash = hash;
         });
-    }
-
-    else {
-        elem.addClass(active);
-        item.each(function(i) {
-            var row = $(this);
-                setTimeout(function() {
-                    row.addClass(play);
-            }, 50*i);
-        });
-    }
-
-});
+      } // End if
+    });
+  });
